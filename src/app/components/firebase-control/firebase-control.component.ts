d418793a5763db2397c8a055b6a4f2ef0a98aa5e
hei <izzie0082005@gmail.com>
2022-10-14 00:52:47 +0100

    beta

--- src/app/components/firebase-control/firebase-control.component.ts
+++ src/app/components/firebase-control/firebase-control.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../../services/firebase.service';
 import { Observable } from 'rxjs';
-
+export interface IHash {
+  [details: string]: string;
+}
 
 @Component({
   selector: 'app-firebase-control',
@@ -14,23 +16,33 @@ export class FirebaseControlComponent {
   collectionDir?: any;
   docSelector?: any;
   user?: any;
+  inputHash: IHash = {};
 
-  constructor(public firebaseService:FirebaseService) {
+  constructor(public firebaseService: FirebaseService) {
+
     this.collectionDir = 'root';
     this.docSelector = {};
     this.selectCollection(this.collectionDir);
     this.ttest();
-    this.firebaseService.getDoc('todo/6bXzb9DG9apl1YF0mLob');
+    
+    this.addInput('s','s')
+    this.addInput('sd','s2')
+    this.addInput('sds','s1')
+    Object.keys(this.inputHash).forEach((key) => {console.log(this.inputHash[key])});
   }
 
   heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
-  addHero(newHero: string) {
-    if (newHero) {
-      this.heroes.push(newHero);
+  addHero(newHero: string) { if (newHero) { this.heroes.push(newHero); } }
+  addInput(inputKey: string, inputValue: string) {
+    if (inputValue && inputKey) {
+      this.inputHash[inputKey] = inputValue;
     }
   }
-  
-  ttest(){
+  getInput(inputKey: string) {
+    return this.inputHash[inputKey];
+  }
+
+  ttest() {
     let array = new Array();
     this.curCollection = this.firebaseService.test('root');
     this.curCollection.subscribe((ref: any) => {
@@ -38,36 +50,37 @@ export class FirebaseControlComponent {
       array = ref;
       console.log('meow');
     });
+    return array;
   }
 
-  onKey(event:any) {const inputValue = event.target.value;}
+  onKey(event: any) { const inputValue = event.target.value; }
 
-  selectCollection(input:any){
+  selectCollection(input: any) {
     this.curCollection = this.getCollection(input);
   }
-  selectDocuemnt(input:any){
+  selectDocuemnt(input: any) {
     this.docSelector = this.firebaseService.getDoc(input).ref;
-    console.log(this.docSelector  )
+    console.log(this.docSelector)
   }
-  getCollection(input?:any){
+  getCollection(input?: any) {
     return this.firebaseService.getCollection(input);
   }
-  saveDoc(input?:any){
-    if(input!=undefined){
-      this.firebaseService.writeDoc(this.collectionDir,input);
-    }else{
-      this.firebaseService.addDoc(this.collectionDir, { 
+  saveDoc(input?: any) {
+    if (input != undefined) {
+      this.firebaseService.writeDoc(this.collectionDir, input);
+    } else {
+      this.firebaseService.addDoc(this.collectionDir, {
         uid: 'undefind',
         name: 'undefind',
         title: 'undefind',
-        content:'undefind',
-       });
+        content: 'undefind',
+      });
     }
   }
-  writeDoc(){
+  writeDoc() {
 
   }
-  deleteDoc(){
+  deleteDoc() {
 
   }
 
